e660c5418e623b37691662de78e199beb29f6a91
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-24 11:31:18 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// arthimatic oprater
+let a = 45;
+let b = 4;
+console.log("a + b =", a+b);
+console.log("a - b =", a-b);
+console.log("a * b =", a*b);
+console.log("a / b =", a/b);
+console.log("a ** b =", a**b);
+console.log("a % b =", a%b);
+
+
+// assignment oprater
+let assignment = 1;
+assignment += 5;
+console.log(assignment)
+
+// comparison oprater
+let comp1 =7;
+let comp2 ="6";
+console.log("comp1 == comp1", comp1 == comp2)
+console.log("comp1 == comp1", comp1 == comp2)
+console.log("comp1 == comp1", comp1 === comp2)
+console.log("comp1 == comp1", comp1 !== comp2)
+
+// logicla oprater
+let x = 4;
+let y = 5;
+console.log(x<y && x==y);
+console.log(x>y || x==y);
